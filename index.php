32a6493f208ed60ba8bf0acbd4258d16c1c8b784
Michael <93167717+McDonaghMichael@users.noreply.github.com>
2023-07-05 10:35:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: Call Button
 Description: Adds a "Call now" button to the bottom of phone screens.
 Version: 1.0
-Author: Your Name
+Author: Michael McDonagh
 */
 
 // Add a submenu in the "Settings" menu
@@ -31,6 +31,11 @@ function call_button_settings_page() {
         update_option('call_button_height', sanitize_text_field($_POST['call_button_height']));
         update_option('call_button_padding', sanitize_text_field($_POST['call_button_padding']));
         update_option('call_button_margin', sanitize_text_field($_POST['call_button_margin']));
+        update_option('call_button_font_size', sanitize_text_field($_POST['call_button_font_size']));
+        update_option('call_button_font_size_type', sanitize_text_field($_POST['call_button_font_size_type']));
+        update_option('call_button_z_index', sanitize_text_field($_POST['call_button_z_index']));
+        update_option('call_button_css', sanitize_textarea_field($_POST['call_button_css']));
+   
         echo '<div class="notice notice-success"><p>Settings saved.</p></div>';
     }
 
@@ -39,10 +44,15 @@ function call_button_settings_page() {
     $call_button_number = get_option('call_button_number', '');
     $call_button_text_color = get_option('call_button_text_color', '#ffffff');
     $call_button_bg_color = get_option('call_button_bg_color', '#000000');
-    $call_button_width = get_option('call_button_width', '200');
+    $call_button_width = get_option('call_button_width', '100');
     $call_button_height = get_option('call_button_height', '50');
     $call_button_padding = get_option('call_button_padding', '10');
     $call_button_margin = get_option('call_button_margin', '10');
+    $call_button_font_size = get_option('call_button_font_size', '16');
+    $call_button_font_size_type = get_option('call_button_font_size_type', 'px');
+    $call_button_z_index = get_option('call_button_z_index', '0'); // Added z-index option
+    $call_button_css = get_option('call_button_css', '');
+
     ?>
 
     <div class="wrap">
@@ -63,8 +73,24 @@ function call_button_settings_page() {
                     <td><input type="text" id="call_button_text_color" name="call_button_text_color" value="<?php echo esc_attr($call_button_text_color); ?>" /></td>
                 </tr>
                 <tr>
+                    <th scope="row"><label for="call_button_font_size">Font Size:</label></th>
+                    <td>
+                        <input type="number" id="call_button_font_size" name="call_button_font_size" min="1" max="100" step="1" value="<?php echo esc_attr($call_button_font_size); ?>" />
+                        <select id="call_button_font_size_type" name="call_button_font_size_type">
+                            <option value="px" <?php selected($call_button_font_size_type, 'px'); ?>>px</option>
+                            <option value="em" <?php selected($call_button_font_size_type, 'em'); ?>>em</option>
+                            <option value="rem" <?php selected($call_button_font_size_type, 'rem'); ?>>rem</option>
+                            <option value="%" <?php selected($call_button_font_size_type, '%'); ?>>%</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
                     <th scope="row"><label for="call_button_bg_color">Background Color:</label></th>
                     <td><input type="text" id="call_button_bg_color" name="call_button_bg_color" value="<?php echo esc_attr($call_button_bg_color); ?>" /></td>
+                </tr>
+                <tr>
+                    <th scope="row"><label for="call_button_z_index">Z-index:</label></th>
+                    <td><input type="number" id="call_button_z_index" name="call_button_z_index" min="0" step="1" value="<?php echo esc_attr($call_button_z_index); ?>" /></td>
                 </tr>
                 <tr>
                     <th scope="row"><label for="call_button_width">Width:</label></th>
@@ -93,6 +119,10 @@ function call_button_settings_page() {
                         <input type="range" id="call_button_margin" name="call_button_margin" min="0" max="50" step="1" value="<?php echo esc_attr($call_button_margin); ?>" />
                         <input type="number" id="call_button_margin_number" name="call_button_margin" min="0" max="50" step="1" value="<?php echo esc_attr($call_button_margin); ?>" />
                     </td>
+                </tr>
+                <tr>
+                    <th scope="row"><label for="call_button_css">Custom CSS:</label></th>
+                    <td><textarea id="call_button_css" name="call_button_css" rows="5"><?php echo esc_textarea(get_option('call_button_css', '')); ?></textarea></td>
                 </tr>
             </table>
 
@@ -132,6 +162,10 @@ function call_button_display_button() {
     $call_button_height = get_option('call_button_height', '50');
     $call_button_padding = get_option('call_button_padding', '10');
     $call_button_margin = get_option('call_button_margin', '10');
+    $call_button_font_size = get_option('call_button_font_size', '16');
+    $call_button_font_size_type = get_option('call_button_font_size_type', 'px');
+    $call_button_z_index = get_option('call_button_z_index', '0');
+    $call_button_css = get_option('call_button_css', '');
 
     if (!empty($call_button_number)) {
         ?>
@@ -143,6 +177,9 @@ function call_button_display_button() {
                 height: <?php echo esc_attr($call_button_height); ?>px;
                 padding: <?php echo esc_attr($call_button_padding); ?>px;
                 margin: <?php echo esc_attr($call_button_margin); ?>px;
+                font-size: <?php echo esc_attr($call_button_font_size); ?><?php echo esc_attr($call_button_font_size_type); ?>;
+                z-index: <?php echo esc_attr($call_button_z_index); ?>;
+                <?php echo esc_html($call_button_css); ?>
             }
         </style>
         <a href="tel:<?php echo esc_attr($call_button_number); ?>" class="call-button"><?php echo esc_html($call_button_text); ?></a>
